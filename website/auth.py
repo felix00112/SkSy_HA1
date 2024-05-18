bc05cc6ab6957163e494e9d544616e8015c1e7ae
felix00112 <felix-hauptmann@gmx.de>
2024-05-18 18:33:59 +0200

    set email requirements down to 2 chars

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def signup():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already registered',category='error')
-        elif len(email) < 4:
+        elif len(email) < 2:
             flash('Email must be at least 6 characters', category='error')
         elif len(first_name) < 3:
             flash('First name must be at least 3 characters', category='error')
